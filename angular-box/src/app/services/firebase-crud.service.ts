8d123baea34791cbaca4476eed2975db0cc21951
Scott Thompson <sthompson1@colgate.edu>
2022-07-23 14:54:15 -0700

    API update

--- angular-box/src/app/services/firebase-crud.service.ts
+++ angular-box/src/app/services/firebase-crud.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { BoxEvent } from '../objects/box-event';
 
@@ -20,10 +20,24 @@ export class FirebaseCRUDService {
     return this.fireservices.collection('test').snapshotChanges();
   }
 
+  read_All_Specific_Box(uid: string){
+    return this.fireservices.collection(uid).snapshotChanges;
+  }
+
   getAllUsers() {
     return new Promise<any>((resolve)=> {
     this.fireservices.collection('test').valueChanges({ idField: 'id' }).subscribe(users => resolve(users));
     })
    }
   
+  add_A_Box(){
+  }
+  
+  add_A_Friend(){
+
+  }
+
+  get_All_Friends(){
+    
+  }
 }
