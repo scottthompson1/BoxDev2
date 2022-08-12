1ae2947eb18154e3f24ea4ec94b28f9c0b2fcaaa
Scott Thompson <sthompson1@colgate.edu>
2022-08-11 19:30:23 -0700

    api updates and box-card for events

---
+++ angular-box/src/app/activity-page/add-event/add-event.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FirebaseCRUDService } from 'src/app/services/firebase-crud.service';
+
+@Component({
+  selector: 'app-add-event',
+  templateUrl: './add-event.component.html',
+  styleUrls: ['./add-event.component.css']
+})
+export class AddEventComponent implements OnInit {
+
+  constructor(private crudService:FirebaseCRUDService) { }
+
+  ngOnInit(): void {
+  }
+
+  addEventToBox(){
+    //placeholder for sending event update to firebase for tooling
+  }
+
+}
